a4c13dd2de468f3f639ee42e618e49727e2ef1cc
Evan Lin <evanslin@gmail.com>
2014-07-23 16:43:53 +0800

    Add concurrency test on web app

--- hello.go
+++ hello.go
@@ -6,6 +6,23 @@ import (
 	"os"
 )
 
+func fabnacci(sum chan int, quit chan int) (ret int) {
+	x, y := 0, 1
+	for {
+		select {
+		case sum <- x:
+			x, y = y, x+y
+			/* is rquivalant with follow
+			z := x + y
+			x = y
+			y = z
+			*/
+		case <-quit:
+			return
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/", hello)
 	fmt.Println("listening...")
@@ -17,4 +34,17 @@ func main() {
 
 func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, world")
+
+	fmt.Fprintln(res, "Init concurrency")
+	//fabnacci
+	fab := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Fprintln(res, "fab =", <-fab)
+		}
+		quit <- 0
+	}()
+
+	fabnacci(fab, quit)
 }
