2b6ac64e9f5d652fa5a49cd0bf04c16443e04536
Evan Lin <evanslin@gmail.com>
2014-07-25 18:28:15 +0800

    Add server response handle for REST by default net/http

--- server.go
+++ server.go
@@ -4,9 +4,91 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 )
 
+// RESTful
+type Resource interface {
+	Get(values url.Values) (int, interface{})
+	Post(values url.Values) (int, interface{})
+	Put(values url.Values) (int, interface{})
+	Delete(values url.Values) (int, interface{})
+}
+
+type (
+	GetNotSupported    struct{}
+	PostNotSupported   struct{}
+	PutNotSupported    struct{}
+	DeleteNotSupported struct{}
+)
+
+func (GetNotSupported) Get(values url.Values) (int, interface{}) {
+	return 405, ""
+}
+
+func (PostNotSupported) Post(values url.Values) (int, interface{}) {
+	return 405, ""
+}
+
+func (PutNotSupported) Put(values url.Values) (int, interface{}) {
+	return 405, ""
+}
+
+func (DeleteNotSupported) Delete(values url.Values) (int, interface{}) {
+	return 405, ""
+}
+
+type API struct{}
+
+func (api *API) requestHandler(resource Resource) http.HandlerFunc {
+	return func(rw http.ResponseWriter, request *http.Request) {
+
+		var data interface{}
+		var code int
+
+		method := request.Method // Get HTTP Method (string)
+		request.ParseForm()      // Populates request.Form
+		values := request.Form
+
+		switch method {
+		case "GET":
+			code, data = resource.Get(values)
+		case "POST":
+			code, data = resource.Post(values)
+		case "PUT":
+			code, data = resource.Put(values)
+		case "DELETE":
+			code, data = resource.Delete(values)
+		}
+
+		content, err := json.Marshal(data)
+		if err != nil {
+			api.Abort(rw, 500)
+		}
+
+		rw.WriteHeader(code)
+		rw.Write(content)
+	}
+}
+
+func (api *API) AddResource(resource Resource, path string) {
+	http.HandleFunc(path, api.requestHandler(resource))
+}
+
+func (api *API) Abort(rw http.ResponseWriter, statusCode int) {
+	rw.WriteHeader(statusCode)
+}
+
+func (api *API) Start() {
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// JSON data
 type Payload struct {
 	Staff Data
 }
@@ -19,45 +101,29 @@ type Data struct {
 type Fruits map[string]int
 type Vagetables map[string]int
 
-func fabnacci(sum chan int, quit chan int) (ret int) {
-	x, y := 0, 1
-	for {
-		select {
-		case sum <- x:
-			x, y = y, x+y
-			/* is rquivalant with follow
-			z := x + y
-			x = y
-			y = z
-			*/
-		case <-quit:
-			return
-		}
-	}
+type HelloResource struct {
+	PostNotSupported
+	PutNotSupported
+	DeleteNotSupported
+}
+
+func (HelloResource) Get(values url.Values) (int, interface{}) {
+	data := map[string]string{"hello": "world"}
+	return 200, data
 }
 
 func main() {
-	http.HandleFunc("/", serveRest)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	if err != nil {
-		panic(err)
-	}
+
+	helloResource := new(HelloResource)
+
+	var api = new(API)
+	api.AddResource(helloResource, "/hello")
+	api.Start()
+
+	//http.HandleFunc("/", requestHandler)
 }
 
 func serveRest(res http.ResponseWriter, req *http.Request) {
-	//fmt.Fprintln(res, "hello, world")
-	// fmt.Fprintln(res, "Init concurrency")
-	// fab := make(chan int)
-	// quit := make(chan int)
-	// go func() {
-	// 	for i := 0; i < 10; i++ {
-	// 		fmt.Fprintln(res, "fab =", <-fab)
-	// 	}
-	// 	quit <- 0
-	// }()
-	// fabnacci(fab, quit)
-
 	//Try json response
 	response, err := getJSONResponse()
 	if err != nil {
